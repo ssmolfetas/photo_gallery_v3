7de1e5784ab77174bbc9d5088a57a02d95342e54
ssmolfetas <akis.molfetas@gmail.com>
2017-01-19 12:53:42 -0500

    Applied HideSeek plugin

--- js/app.js
+++ js/app.js
@@ -2,4 +2,15 @@ lightbox.option({
     'imageFadeDuration': 300,
     'alwaysShowNavOnTouchDevices': true,
     'maxHeight': 600
-})
+});
+
+$('#search').hideseek({
+      list:           '.hideseek-data',
+      nodata:         'Sorry, No results found.',
+      attribute:      'text',
+      highlight:      true,
+      ignore:         '',
+      navigation:     true,
+      ignore_accents: false
+});
+
